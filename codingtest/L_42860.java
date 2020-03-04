84c5dac15f704e1f78700349bf331ab957418d78
bloodfinger <didwodn8822@gmail.com>
2020-03-04 18:19:51 +0900

    commit

---
+++ codingtest/L_42860.java
@@ -0,0 +1,23 @@
+package codingtest;
+
+public class L_42860 {
+
+	
+	
+	public int solution(String name) {
+        int answer = 0;
+        
+        
+        
+        
+        
+        return answer;
+    }
+	
+	
+	public static void main(String[] args) {
+		L_42860 l = new L_42860();
+		l.solution("JEROEN");
+	}
+
+}
--- codingtest/L_42883.java
+++ codingtest/L_42883.java
@@ -1,62 +1,41 @@
 package codingtest;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class L_42883 {
 	
 	public String solution(String number, int k) {
         String answer = "";
-        List<Integer> list = new LinkedList<Integer>();
-        int retail = number.length()-k;
-       
-        String[] ch = number.split("");
-		int[] arr = new int[ch.length];
-		int j =0;
-		 
-		for (String string : ch) {
-			arr[j]=Integer.parseInt(string);
-			j++;
+        char[] result = new char[number.length() -k];
+        Stack<Character> st = new Stack<Character>();
+        
+        for (int i = 0; i < number.length(); i++) {
+			char c = number.charAt(i);
+			while (!st.isEmpty()   &&   st.peek() < c   &&   k-- > 0) { //st에 값이 있을때
+				st.pop();
+			}
+			st.push(c);
 		}
-		 
-		permutation(arr, 0, arr.length, 2, list);
-		list.sort(null);
-		System.out.println(list.get(list.size()-1));
+        
+        System.out.println(st);
+//		for (int i = 0; i < result.length; i++) {
+//			result[i] = st.get(i);
+//			System.out.print(result[i]);
+//		}
 		
-		System.out.println(list);
-		answer = list.get(list.size()-1)+"";
-        return answer;
+        return new String(result);
     }
 	
 	
-	static void permutation(int[] arr, int depth, int n, int r, List<Integer> list) {
-	    if(depth == r) {
-	    	String k = "";
-	        for(int i=0; i<r; i++) {
-	            k = k + arr[i];
-	        }
-	        list.add(Integer.parseInt(k));
-	        return;
-	    }
-	 
-	    for(int i=depth; i<n; i++) {
-	        swap(arr, depth, i);
-	        permutation(arr, depth + 1, n, r, list);
-	        swap(arr, depth, i);
-	    }
-	}
-		 
-	static void swap(int[] arr, int depth, int i) {
-	    int temp = arr[depth];
-	    arr[depth] = arr[i];
-	    arr[i] = temp;
-	}
 	
 	
 
 	public static void main(String[] args) {
 		L_42883 l = new L_42883();
-		l.solution("1924", 2);
+		l.solution("4177252841", 4);
 	}
 
 }
