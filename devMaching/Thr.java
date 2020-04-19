512329d5f1de311076760b9c9f49034c019175d9
bloodfinger <didwodn8822@gmail.com>
2020-04-19 15:38:50 +0900

    update commit

--- devMaching/Thr.java
+++ devMaching/Thr.java
@@ -3,33 +3,40 @@ package devMaching;
 public class Thr {
 
 	public int solution(int[][] office, int r, int c, String[] move) {
-	      int answer = 0;
-//	      int r2 = r;
-//	      int c3 = c;
+	      int answer = office[r][c];
+	      office[r][c] = 0;
 	      
+	      int dir = 1; //동:2, 서:4, 남:3, 북:1 
+	      int testR = r;
+    	  int testC = c;
 	      
-	      for (int i = 0; i < move.length; i++) { // move의 명령의 수까지
-	    	  int mybot = office[r][c]; //이 값을 통해서 비교
-	    	  
-	    	  if("go".equals(move[i])) { // 현재 바라보고 있는 방향으로 한 칸 전진
-	    		  
-	    		  if(true) { //격자를 벗어나는 경우
-	    			  
-	    		  }else if() { //물건이 있는 경우
-	    			  
-	    		  }else { //문제 없는경우
-	    			  answer += office[r][c];
-	    			  office[r][c] = 0;
-	    			  //r을 증가시킬지 c를 증가시킬지 방향을 통해서 결정
-	    		  }
-	    	  }else if("right".equals(move[i])) { //방향 전환 오른쪽으로
-	    		  
-	    	  }else if("left".equals(move[i])) { //방향 전환 왼쪽으로
-	    		  
-	    	  }
-	      }
-	      
-	      
+			for (int i = 0; i < move.length; i++) { // move의 명령의 수까지
+				if ("go".equals(move[i])) { // 현재 바라보고 있는 방향으로 한 칸 전진
+					if (dir == 1) testR--;
+					else if (dir == 2) testC++;
+					else if (dir == 3) testR++;
+					else if (dir == 4) testC--;
+					System.out.println("r : " + testR + ", c : " + testC );
+					if((testR >= 0 && testR < 3 && testC >= 0 && testC < 3)&&(office[testR][testC] != -1)) {
+						r = testR ;
+						c = testC ;
+						answer += office[r][c];
+						office[r][c] = 0;
+					}else {
+						testR = r;
+						testC = c;
+					}
+				} else if ("right".equals(move[i])) { // 방향 전환 오른쪽으로
+					// 방향 전환
+					dir = dir % 4 + 1;
+				} else if ("left".equals(move[i])) { // 방향 전환 왼쪽으로
+					if(dir == 1) {
+						dir = 4;
+					}else {
+						dir = (dir - 1) % 4;
+					}
+				} 
+			}
 	      return answer;
 	 }
 	
